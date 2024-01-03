3acd64fc175a48030a77791d345295b581db3167
Shishir Kumar <sky8052785942ocean@gmail.com>
2024-01-04 03:46:15 +0530

    update count-occurence with split and arrow func

--- count-occurences/count-occurences.js
+++ count-occurences/count-occurences.js
@@ -1,14 +1,18 @@
-function countOccurences(str, char) {
-	let count = 0; 
+// function countOccurences(str, char) {
+// 	let count = 0; 
 
-	for(let i = 0; i < str.length; i++) {
-		// console.log(str[i]);
-		if(str[i] === char) {
-			count++;
-		}
-	}
+// 	for(let i = 0; i < str.length; i++) {
+// 		// console.log(str[i]);
+// 		if(str[i] === char) {
+// 			count++;
+// 		}
+// 	}
 
-	return count;
-}
+// 	return count;
+// }
+/*
+	shortest way using split and arrow func
+*/
+const countOccurences = (str, char) => str.split(char).length - 1;
 
 module.exports = countOccurences; 
